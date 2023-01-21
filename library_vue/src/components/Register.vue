afdbefb618fdce5913e227285b988a529d233279
lydsera <1048554745@qq.com>
2023-01-21 16:37:43 +0800

    注册

--- library_vue/src/components/Register.vue
+++ library_vue/src/components/Register.vue
@@ -2,8 +2,8 @@
     <div  class="login_area">
             <div style="font-size: 30px; text-align: center; padding: 30px 0; color: #333">欢迎注册</div>
             <el-form ref="form" :rules="rules" :model="form">
-                <el-form-item prop="id">
-                    <el-input  v-model="form.id" placeholder="请输入用户名" oninput="value=value.replace(/[^\d]/g,'')"></el-input>
+                <el-form-item prop="name">
+                    <el-input  v-model="form.name" placeholder="请输入用户名" ></el-input>
                 </el-form-item>
                 
                 <el-form-item prop="pwd">
@@ -12,12 +12,12 @@
                 </el-form-item>
                 
                 <el-form-item prop="again_pwd">
-                    <el-input  v-model="form.pwd" show-password
+                    <el-input  v-model="form.again_pwd" show-password
                             placeholder="请再次输入密码..."></el-input>
                 </el-form-item>
                 
                 <el-form-item>
-                    <el-button style="width: 100%;background-color: #00b385;:" type="primary" @click="login">注册</el-button>
+                    <el-button style="width: 100%;background-color: #00b385;:" type="primary" @click="reg">注册</el-button>
                 </el-form-item>
             </el-form>
             <div style="text-align:left">
@@ -34,7 +34,7 @@
 
 export default 
 {
-    name: "Login",
+    name: "reg",
     data () 
     {
         return {
@@ -46,15 +46,21 @@ export default
             },
             rules:
             {
-                id: 
+                name: 
                 [
-                    { required: true, message: '请输入用户ID', trigger: 'blur' },
+                    { required: true, message: '请输入用户名', trigger: 'blur' },
                     { min: 1, max: 10, message: '长度在 1 到 10 个字符', trigger: 'blur' },
                     
                 ],
                 pwd:
                 [
                     { required: true, message: '请输入用户密码', trigger: 'blur' },
+                    { min: 5, max: 10, message: '长度在 5 到 10 个字符', trigger: 'blur' },
+                    
+                ],
+                again_pwd:
+                [
+                    { required: true, message: '请再次输入用户密码', trigger: 'blur' },
                     { min: 5, max: 10, message: '长度在 5 到 10 个字符', trigger: 'blur' },
                     
                 ],
@@ -76,28 +82,39 @@ export default
     },
     methods:
     {
-        login()
+        reg()
         {
             this.$refs.form.validate((valid) => {
                 if (valid) 
                 {
-                    let f = 
+                    if(this.form.pwd==this.form.again_pwd)
                     {
-                        id: parseInt(this.form.id),
-                        password: this.form.pwd
+                        let f = 
+                        {
+                            name: this.form.name,
+                            password: this.form.pwd
+                        }
+                        alert('submit!');
+                        this.$axios.post('http://127.0.0.1:3333/reg/',f
+                        ).then(res=>
+                            {
+                                alert('注册成功，请牢记你的id：'+res.data.id);
+                                console.log(res);
+                                sessionStorage.setItem("user", JSON.stringify(res.data))
+                                this.$router.push('/reader');
+                            }
+                        ).catch(res=>
+                            {
+                                console.log(res);
+                            }
+                        )
                     }
-                    alert('submit!');
-                    this.$axios.post('http://127.0.0.1:3333/login/',f
-                    ).then(res=>
-                        {
-                            console.log(res);
-                            sessionStorage.setItem("user", JSON.stringify(res.data))
-                        }
-                    ).catch(res=>
-                        {
-                            console.log(res);
-                        }
-                    )
+                    else{
+                        this.$message.error("两次密码不一致！");
+                        console.log('error submit!!');
+                    }
+                    
+                    
                 } 
                 else 
                 {
